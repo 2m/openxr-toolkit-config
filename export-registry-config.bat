d53abc18d8c9895b85c2de8b947ce06e75ec4c54
Martynas Mickevičius <self@2m.lt>
2025-02-03 18:07:22 +0200

    convert to utf8

--- export-registry-config.bat
+++ export-registry-config.bat
@@ -1,1 +1,2 @@
-reg export HKEY_CURRENT_USER\Software\OpenXR_Toolkit openxr_toolkit_app_settings.txt
+reg export HKEY_CURRENT_USER\Software\OpenXR_Toolkit openxr_toolkit_app_settings.reg /y
+powershell -command "Get-Content openxr_toolkit_app_settings.reg -Encoding Oem | Out-File openxr_toolkit_app_settings.txt -Encoding utf8"
